db8d1f1edd0d2cc8785651fe4e6af059a2af2207
Tuhin Ghose <tuhin.ghose@mail.utoronto.ca>
2023-10-28 00:31:20 -0400

    feat: implemented login required section

--- project/app.py
+++ project/app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+from functools import wraps
 
 basedir = Path(__file__).resolve().parent
 
@@ -25,6 +25,15 @@ app.config.from_object(__name__)
 db = SQLAlchemy(app)
 
 from project import models
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            flash('Please log in.')
+            return jsonify({'status': 0, 'message': 'Please log in.'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/')
@@ -71,6 +80,7 @@ def logout():
 
 
 @app.route('/delete/<int:post_id>', methods=['GET'])
+@login_required
 def delete_entry(post_id):
     """Deletes post from database."""
     result = {'status': 0, 'message': 'Error'}
