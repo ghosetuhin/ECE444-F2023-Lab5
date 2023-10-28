7c3d288e0c0892ef6bd4b115fcb4069d1c5c4acf
Tuhin Ghose <tuhin.ghose@mail.utoronto.ca>
2023-10-27 23:49:46 -0400

    feat: implemented javascript section

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 from pathlib import Path
-
+import json
 from project.app import app, init_db
 
 TEST_DB = "test.db"
@@ -72,3 +72,10 @@ def test_messages(client):
     assert b"No entries here so far" not in rv.data
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
+
+
+def test_delete_message(client):
+    """Ensure the messages are being deleted"""
+    rv = client.get('/delete/1')
+    data = json.loads(rv.data)
+    assert data["status"] == 1
